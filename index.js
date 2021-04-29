acd1af1370ea61ed7a037486afb94620a8e57224
Tijani Fulani <Fulani07@gmail.com>
2021-04-29 15:06:26 +0100

    promt user for id

--- index.js
+++ index.js
@@ -53,4 +53,8 @@ Now that you can add a todo, add the option to either delete or update a todo. A
 
 
 // Prompt the user for a user id
+
+const userId = prompt('Please give me a user id from 1 to 10')
+console.log(`Given user id: ${userId}`)
+
 // Display an alert with the username and all the todos titles that belong to that user 
